75e0c238abefa6b2aefdc2e060f9da5dade0c267
Filip Dorian Pindej <filip.pindej@gmail.com>
2020-01-09 14:58:19 +0100

    Implemented Pipe generator

--- FiltersAndPipesDemo/Pipe.cs
+++ FiltersAndPipesDemo/Pipe.cs
@@ -6,6 +6,9 @@ namespace FiltersAndPipesDemo
     {
         ConcurrentQueue<Order> queue = new ConcurrentQueue<Order>();
         public bool HasMesage => !queue.IsEmpty;
+
+        public bool HasMessage { get; internal set; }
+
         public Order GetMessage() {
             if (queue.TryDequeue(out Order order)) return order;
             else throw new System.Exception("Can't dequeue");
---
+++ FiltersAndPipesDemo/Pipeline.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+
+namespace FiltersAndPipesDemo
+{
+    class Pipeline
+    {
+        public Pipe InPipe { get; private set; }
+        public Pipe OutPipe { get; private set; }
+
+        public Collection<FilterBase> pipes = new Collection<FilterBase>();
+
+        public void AddFilter<T>(int count) where T: FilterBase, new()
+        {
+            var nextPipe = new Pipe();
+
+            for(int i = 0; i < count; ++i)
+            {
+                pipes.Add(new T()
+                {
+                    InPipe = OutPipe,
+                    OutPipe = nextPipe
+                });
+            }
+
+            OutPipe = nextPipe;
+        }
+    }
+}
