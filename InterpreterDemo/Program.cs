afbfd9bb671e9e4d7a72762f0d5ac8fbedd02360
Filip Dorian Pindej <filip.pindej@gmail.com>
2020-01-09 15:41:38 +0100

    Added difference operation and used it like 2x+y-z

--- InterpreterDemo/Program.cs
+++ InterpreterDemo/Program.cs
@@ -10,16 +10,19 @@ namespace InterpreterDemo
     {
         static void Main(string[] args)
         {
-            var expression = new Sum(
-                new Product(
-                    new Literal(2),
-                    new Variable("x")),
-                new Variable("y"));
+            var expression = new Difference(
+                new Sum(
+                    new Product(
+                        new Literal(2),
+                        new Variable("x")),
+                    new Variable("y")),
+                new Variable("z"));
 
             Console.WriteLine(expression.Interpret(
                 new Dictionary<string, double>() {
                     { "x", 2 },
-                    { "y", 3 }
+                    { "y", 3 },
+                    { "z", 4 }
                 }));
         }
     }
